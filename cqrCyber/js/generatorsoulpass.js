c58730068e187d9eb0f20557ed64c1ce7396c5db
Umar Farooq <umarcbs.uc@users.noreply.github.com>
2020-09-14 09:30:10 +0530

    Added new files

---
+++ cqrCyber/js/generatorsoulpass.js
@@ -0,0 +1,16 @@
+console.log("Script linked");
+var keyList = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var temp = '';
+
+function generatePass(pLenght) {
+    temp = '';
+	for(var i=0;i<pLenght;i++){
+		temp += keyList.charAt(Math.floor(Math.random() * keyList.length))
+	}
+	return temp;
+}
+
+function populate(enterLenght){
+	document.passGen.output.value = generatePass(enterLenght);
+}
+
