e633401d46004e924118e61fa03731c7c6a84ab0
Umar Farooq <umarcbs.uc@users.noreply.github.com>
2020-10-11 11:44:30 +0530

    added

---
+++ web-version.php
@@ -0,0 +1,17 @@
+<body onload="setTimeout(Umar, 0);" oncontextmenu="return false" onkeydown="return false" bgcolor="black">
+
+<script>
+  function Umar()
+    {
+    if(screen.width > screen.height)
+      {var oW = screen.height;}
+    else
+      {var oW = screen.width;}
+
+if(oW <= 480)
+    top.window.location.href="mobile.php";
+    else window.location="web-version.php";
+    }
+</script>
+<title>Web Version</title><style>body{padding-top: 2%;} ::selection {color:red;background:black;} ::-moz-selection {color:red;background:black;}</style><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body bgcolor=efefef link=red vlink=red><center><h3><span style="color:#9c353a">WEB VERSION</span></h3><hr noshade>
