1a417b0e7e8142169d67f453de801597fec1d8b3
Umar Farooq <63138841+umarcbs@users.noreply.github.com>
2021-03-06 20:46:40 +0530

    added

---
+++ 1_files/translation-fonts.js
@@ -0,0 +1,81 @@
+
+function myFunctionAmiri() {
+  document.getElementById("demo").style.fontFamily = "Amiri";
+  document.getElementById("demo1").style.fontFamily = "Amiri";
+  document.getElementById("demo2").style.fontFamily = "Amiri";
+  document.getElementById("demo3").style.fontFamily = "Amiri";
+  document.getElementById("demo4").style.fontFamily = "Amiri";
+  document.getElementById("demo5").style.fontFamily = "Amiri";
+  document.getElementById("demo6").style.fontFamily = "Amiri";
+}
+
+function myFunctionArefRuqaa() {
+  document.getElementById("demo").style.fontFamily = 'Aref Ruqaa';
+  document.getElementById("demo1").style.fontFamily = 'Aref Ruqaa';
+  document.getElementById("demo2").style.fontFamily = 'Aref Ruqaa';
+  document.getElementById("demo3").style.fontFamily = 'Aref Ruqaa';
+  document.getElementById("demo4").style.fontFamily = 'Aref Ruqaa';
+  document.getElementById("demo5").style.fontFamily = 'Aref Ruqaa';
+  document.getElementById("demo6").style.fontFamily = 'Aref Ruqaa';
+}
+
+function myFunctionCairo() {
+  document.getElementById("demo").style.fontFamily = "Cairo";
+  document.getElementById("demo1").style.fontFamily = "Cairo";
+  document.getElementById("demo2").style.fontFamily = "Cairo";
+  document.getElementById("demo3").style.fontFamily = "Cairo";
+  document.getElementById("demo4").style.fontFamily = "Cairo";
+  document.getElementById("demo5").style.fontFamily = "Cairo";
+  document.getElementById("demo6").style.fontFamily = "Cairo";
+}
+
+function myFunctionKufam() {
+  document.getElementById("demo").style.fontFamily = 'Kufam';
+  document.getElementById("demo1").style.fontFamily = 'Kufam';
+  document.getElementById("demo2").style.fontFamily = 'Kufam';
+  document.getElementById("demo3").style.fontFamily = 'Kufam';
+  document.getElementById("demo4").style.fontFamily = 'Kufam';
+  document.getElementById("demo5").style.fontFamily = 'Kufam';
+  document.getElementById("demo6").style.fontFamily = 'Kufam';
+}
+
+function myFunctionKatibeh() {
+  document.getElementById("demo").style.fontFamily = 'Katibeh';
+  document.getElementById("demo1").style.fontFamily = 'Katibeh';
+  document.getElementById("demo2").style.fontFamily = 'Katibeh';
+  document.getElementById("demo3").style.fontFamily = 'Katibeh';
+  document.getElementById("demo4").style.fontFamily = 'Katibeh';
+  document.getElementById("demo5").style.fontFamily = 'Katibeh';
+  document.getElementById("demo6").style.fontFamily = 'Katibeh';
+}s
+
+function myFunctionLemonada() {
+  document.getElementById("demo").style.fontFamily = 'Lemonada';
+  document.getElementById("demo1").style.fontFamily = 'Lemonada';
+  document.getElementById("demo2").style.fontFamily = 'Lemonada';
+  document.getElementById("demo3").style.fontFamily = 'Lemonada';
+  document.getElementById("demo4").style.fontFamily = 'Lemonada';
+  document.getElementById("demo5").style.fontFamily = 'Lemonada';
+  document.getElementById("demo6").style.fontFamily = 'Lemonada';
+}
+
+function myFunctionMarkazi() {
+  document.getElementById("demo").style.fontFamily = 'Markazi Text';
+  document.getElementById("demo1").style.fontFamily = 'Markazi Text';
+  document.getElementById("demo2").style.fontFamily = 'Markazi Text';
+  document.getElementById("demo3").style.fontFamily = 'Markazi Text';
+  document.getElementById("demo4").style.fontFamily = 'Markazi Text';
+  document.getElementById("demo5").style.fontFamily = 'Markazi Text';
+  document.getElementById("demo6").style.fontFamily = 'Markazi Text';
+
+}
+
+function myFunctionScheherazade() {
+  document.getElementById("demo").style.fontFamily = "Scheherazade";
+  document.getElementById("demo1").style.fontFamily = "Scheherazade";
+  document.getElementById("demo2").style.fontFamily = "Scheherazade";
+  document.getElementById("demo3").style.fontFamily = "Scheherazade";
+  document.getElementById("demo4").style.fontFamily = "Scheherazade";
+  document.getElementById("demo5").style.fontFamily = "Scheherazade";
+  document.getElementById("demo6").style.fontFamily = "Scheherazade";
+}
